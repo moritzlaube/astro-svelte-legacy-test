6d0e02962fa708754a7b94d7535befb5ac168f9d
Moritz Laube <info@moritz-laube.de>
2022-01-04 11:55:06 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import legacy from "@vitejs/plugin-legacy";
+
+export default {
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // pages: './src/pages', // Path to Astro components, pages, and data
+  // dist: './dist',       // When running `astro build`, path to final static output
+  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  buildOptions: {
+    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true, // Generate sitemap (set to "false" to disable)
+  },
+  devOptions: {
+    // hostname: 'localhost',  // The hostname to run the dev server on.
+    // port: 3000,             // The port to run the dev server on.
+  },
+  vite: {
+    build: {
+      target: "es2015",
+    },
+    plugins: [
+      legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+    ],
+  },
+  renderers: ["@astrojs/renderer-svelte"],
+};
